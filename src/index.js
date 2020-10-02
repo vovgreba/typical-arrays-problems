74a0722925cf329e2fd87a6c860cf6b3919e0e43
vovgreba <49303544+vovgreba@users.noreply.github.com>
2020-10-03 01:40:35 +0300

    feat: add functions w index.html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
+  if(array && array.length !== 0) {
+    return Math.min.apply( Math, array );
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if(array && array.length !== 0) {
+    return Math.max.apply( Math, array );
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var sum = 0;
+  if(array && array.length !== 0) {
+    for(var i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    return sum / array.length;
+  } 
+    return 0;
+  
 }
+
